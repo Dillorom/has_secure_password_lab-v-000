32a0c5b0928ee74c659ae52ecb029e01c94f10f9
Dillorom Abdulloeva <dabdulloeva@gmail.com>
2018-08-10 18:16:33 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,10 @@ class UsersController < ApplicationController
   def create
     if user = User.new(user_params).save
       session[:user_id] = user.id
-     redirect_to root_path
+      redirect_to root_path
+    else
+      redirect_to new_user_path
+    end
   end
 
    private
