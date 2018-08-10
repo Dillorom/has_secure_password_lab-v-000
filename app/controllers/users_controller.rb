84219dbd3de3ab4f25ef5b4b363828bf65fa56e8
Dillorom Abdulloeva <dabdulloeva@gmail.com>
2018-08-10 17:49:54 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,9 +6,9 @@ class UsersController < ApplicationController
   def create
      user = User.new(user_params).save
    end
-  
+
    private
-  
+
    def user_params
      params.require(:user).permit(:name, :password, :password_confirmation)
    end
