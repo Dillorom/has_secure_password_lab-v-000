ad6c9f593ddb0ecec99587d0361d094a42cfca95
Dillorom Abdulloeva <dabdulloeva@gmail.com>
2018-08-10 17:10:15 +0000

    automatically backed up by learn

--- app/controllers/concerns/sessions_controller.rb
+++ app/controllers/concerns/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
 
   def create
     @user = User.find(params[:username] => params[:username])
-    session[:user_id] = user.id 
+    session[:user_id] = user.id
     redirect_to root_path
   end
 end
